4c6e7412c865c609e9c905a519a401536668ad10
Antogin <georges.indrianjafy@contentsquare.com>
2021-12-14 09:06:17 +0100

    lint

--- src/app/Logo.vue
+++ src/app/Logo.vue
@@ -1,7 +1,9 @@
 <template functional>
-  <div class="d-flex align-items-end logo-container"
-       :class="data.staticClass || ''"
-       v-bind="data.attrs">
+  <div
+    class="d-flex align-items-end logo-container"
+    :class="data.staticClass || ''"
+    v-bind="data.attrs"
+  >
     <!-- <svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="5.74 31.24 123.89 72.89">
       <g transform="matrix(1.0344 0 0 1.0869 -2.068 -181.521)">
         <rect width="5.994" height="23.366" x="9.929" y="224.55" rx="2.997" ry="2.997" />
@@ -24,9 +26,9 @@
   </div>
 </template>
 <style scoped>
-  svg {
-    fill: var(--primary);
-    height: 32px;
-    margin-bottom: 2px;
-  }
+svg {
+  fill: var(--primary);
+  height: 32px;
+  margin-bottom: 2px;
+}
 </style>
