5da14f567eb6b3fd5d9d9091f2892dcb1512eab5
Antogin <georges.indrianjafy@contentsquare.com>
2021-12-14 09:05:02 +0100

    personal

--- src/app/Logo.vue
+++ src/app/Logo.vue
@@ -2,7 +2,7 @@
   <div class="d-flex align-items-end logo-container"
        :class="data.staticClass || ''"
        v-bind="data.attrs">
-    <svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="5.74 31.24 123.89 72.89">
+    <!-- <svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="5.74 31.24 123.89 72.89">
       <g transform="matrix(1.0344 0 0 1.0869 -2.068 -181.521)">
         <rect width="5.994" height="23.366" x="9.929" y="224.55" rx="2.997" ry="2.997" />
         <rect width="5.994" height="41.989" x="19.849" y="215.2" rx="2.997" ry="2.819" />
@@ -20,7 +20,7 @@
     </svg>
     <span class="text-body ml-2 text-nowrap">airsonic&nbsp;
       <span class="text-muted">(refix)</span>
-    </span>
+    </span> -->
   </div>
 </template>
 <style scoped>
