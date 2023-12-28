37790a12613d2019b8457eea7e83e36049ddad4a
Thomas Amland <thomas.amland@gmail.com>
2023-12-28 13:20:22 +0100

    dont create links for artists without id

--- src/library/track/CellArtist.vue
+++ src/library/track/CellArtist.vue
@@ -2,9 +2,12 @@
   <td class="d-none d-lg-table-cell">
     <template v-for="(artist, index) in props.track.artists">
       <span v-if="index > 0" :key="artist.id">, </span>
-      <router-link :key="`${artist.id}-link`" :to="{name: 'artist', params: { id: artist.id }}">
+      <router-link v-if="artist.id" :key="`${artist.id}-link`" :to="{name: 'artist', params: { id: artist.id }}">
         {{ artist.name }}
       </router-link>
+      <template v-else>
+        {{ artist.name }}
+      </template>
     </template>
   </td>
 </template>
