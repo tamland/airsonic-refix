72d9a1d90d18a4054b79473419d3dbf7bcc21079
Thomas Amland <thomas.amland@gmail.com>
2023-12-28 12:38:44 +0100

    add missing file

---
+++ src/shared/components/SwitchInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="custom-control custom-switch">
+    <input
+      :id="id"
+      type="checkbox"
+      class="custom-control-input"
+      :checked="value"
+      @change="$emit('input', $event.target.checked)"
+    >
+    <label :for="id" class="custom-control-label" />
+  </div>
+</template>
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import { uniqueId } from 'lodash-es'
+
+  export default defineComponent({
+    props: {
+      value: { type: Boolean, default: false },
+    },
+    computed: {
+      id(): string {
+        return uniqueId('switch-')
+      }
+    },
+  })
+</script>
